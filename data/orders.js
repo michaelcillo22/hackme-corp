f2c9b343cd74c924ad7dcc3d3294385181a07b34
Michael Cillo <mjcillo22@gmail.com>
2025-03-23 15:18:46 -0400

    added gameplan for data/orders.js

---
+++ data/orders.js
@@ -0,0 +1,22 @@
+import { ordersData } from '../config/mongoCollections.js';
+import { ObjectId } from 'mongodb';
+
+const ordersExample = [
+    {},
+    {},
+    {},
+    // ... other orders
+];
+
+
+// TODO: getOrderById(orderId)
+
+// TODO: createOrder()
+
+// TODO: getAllOrdersByVendor(vendorId)
+
+// TODO: removeOrder()
+
+// TODO: updateOrder()
+
+// TODO:
