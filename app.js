3b4717d1293083ff30a3c79f2f49df357ba7a7e1
Richard Devitt <richyrychporter@hotmail.com>
2025-03-27 18:01:40 -0400

    Added users collection along with helper file and seed file

---
+++ app.js
@@ -0,0 +1,32 @@
+
+import { dbConnection, closeConnection } from './config/mongoConnection.js';
+
+import express from 'express';
+import { seedDB } from './seed.js';
+const app = express();
+// import configRoutes from './routes/index.js';
+
+
+
+(async () => {
+    const db = await dbConnection();
+    console.log('Database connected');
+
+    try {
+        await seedDB();
+
+    } catch (e) {
+        console.error('Errors seeding or server setup:', e);
+    } finally {
+        await closeConnection();
+    }
+
+app.use(express.json());
+
+// configRoutes(app);
+
+app.listen(3000, () => {
+    console.log('Ive now got a server!');
+    console.log('My routes will be running on http://localhost:3000');
+});
+})();
