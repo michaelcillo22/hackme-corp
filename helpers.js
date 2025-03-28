f22f6c523ba84e074a303c8d16db6227207d6aa7
christendeo <christen.deocampo@gmail.com>
2025-03-28 05:42:59 -0400

    Updated helpers methods

--- helpers.js
+++ helpers.js
@@ -3,6 +3,7 @@ import {ObjectId} from "mongodb";
 
 const exportedMethods = {
     checkId(id, varName) {
+
         // Check if an id is provided
         if (!id) {
             throw `Error: You must provide a ${varName}`;
@@ -28,7 +29,9 @@ const exportedMethods = {
 
         return id;
     },
+
     checkString(strVal, varName) {
+
         // Check if a string is provided
         if (!strVal) {
             throw `Error: You must supply a ${varName}!`;
@@ -53,7 +56,46 @@ const exportedMethods = {
         }
 
         return strVal;
-    }
+    },
+
+    checkValidURL(url, varName) {
+
+        // Check using regex
+        let urlRegex = /^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w-]*)*$/;
+
+        if (!urlRegex.test(url)) {
+            throw `Error: ${url} is not a valid ${varName}`;
+        }
+
+        return url;
+    },
+
+    checkStringArray(arr, varName) {
+
+        // Check if array is provided and if it's a valid one
+        if (!arr || !Array.isArray(arr)) {
+            throw `You must provide an array of ${varName}`;
+        }
+        
+        // Iterate through the array
+        for (let i in arr) {
+            if (typeof arr[i] !== 'string' || arr[i].trim().length === 0) {
+                throw `One or more elements in ${varName} array is not a string or is an empty string`;
+            }
+
+            // Check if each one is a valid URL
+            if (!this.checkValidURL(arr[i], "URL")) {
+                throw `${arr[i]} is not a valid URL`;
+            }
+
+            // Trim each one
+            arr[i] = arr[i].trim();
+        }
+    
+        return arr;
+    },
+
+
 };
 
 export default exportedMethods;
