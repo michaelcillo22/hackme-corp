6e45be462b0f8cca7371d343e2378273c4db9e3b
rrouge33 <rruggier1@stevens.edu>
2025-03-29 20:42:51 -0400

    Update checkout.js

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,56 +1,53 @@
 import { Router } from 'express';
-const router = Router();
-//import ./data/order.js
-//import ./data/product.js
-//import ./data/user.js
+import Stripe from 'stripe';    // stripe payment processing platform
 
-router.get('/checkout', isLoggedIn, function (req, res, next) {
+const router = Router();
+const stripe = new Stripe(stripeSecretKey);
+
+router.get('/checkout', isLoggedIn, (req, res) => {
   if (!req.session.cart) {
     return res.redirect('/shopping-cart');
   }
   const cart = new Cart(req.session.cart);
   const errMsg = req.flash('error')[0];
+
   return res.render('shop/checkout', {
     total: cart.totalPrice,
-    errMsg: errMsg,
+    errMsg,
     noError: !errMsg,
   });
 });
 
-router.post('/checkout', isLoggedIn, function (req, res, next) {
+router.post('/checkout', isLoggedIn, async (req, res) => {
   if (!req.session.cart) {
     return res.redirect('/shopping-cart');
   }
   const cart = new Cart(req.session.cart);
 
-  const stripe = require('stripe')(stripeSecretKey);
-
-  stripe.charges.create(
-    {
+  try {
+    const charge = await stripe.charges.create({
       amount: cart.totalPrice * 100,
       currency: 'usd',
-      source: req.body.stripeToken, // obtained with Stripe.js
+      source: req.body.stripeToken, // obtained from Stripe.js
       description: 'Test Charge',
-    },
-    function (err, charge) {
-      if (err) {
-        req.flash('error', err.message);
-        return res.redirect('/checkout');
-      }
-      const order = new Order({
-        user: req.user,
-        cart: cart,
-        address: req.body.address,
-        name: req.body.name,
-        paymentId: charge.id,
-      });
-      order.save(function (err, result) {
-        req.flash('success', 'Successfully bought product!');
-        req.session.cart = null;
-        res.redirect('/');
-      });
-    },
-  );
+    });
+
+    const order = new Order({
+      user: req.user,
+      cart,
+      address: req.body.address,
+      name: req.body.name,
+      paymentId: charge.id,
+    });
+
+    await order.save();
+    req.flash('success', 'Successfully bought product!');
+    req.session.cart = null;
+    res.redirect('/');
+  } catch (err) {
+    req.flash('error', err.message);
+    res.redirect('/checkout');
+  }
 });
 
 function isLoggedIn(req, res, next) {
