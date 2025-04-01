1fed40ea66eef201ab2a25b26d58b2232a216ffd
christendeo <christen.deocampo@gmail.com>
2025-03-31 20:27:02 -0400

    Update

--- config/mongoCollections.js
+++ config/mongoCollections.js
@@ -18,5 +18,7 @@ const getCollectionFn = (collection) => {
 /* Now, you can list your collections here:
 NOTE: YOU WILL NEED TO CHANGE THE CODE BELOW TO HAVE THE COLLECTION(S) REQUIRED BY THE ASSIGNMENT */
 // TODO: Add collections here.
+
+export const users = getCollectionFn('users');
 export const ordersData = getCollectionFn('orders');
-export const productsData = getCollectionFn('products');
+
