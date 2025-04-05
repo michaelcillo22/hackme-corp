7f87930b8735516de41b22f4f094929da856c925
christendeo <christen.deocampo@gmail.com>
2025-04-04 21:57:15 -0400

    Updating collection

--- config/mongoCollections.js
+++ config/mongoCollections.js
@@ -19,6 +19,6 @@ const getCollectionFn = (collection) => {
 NOTE: YOU WILL NEED TO CHANGE THE CODE BELOW TO HAVE THE COLLECTION(S) REQUIRED BY THE ASSIGNMENT */
 // TODO: Add collections here.
 
-export const ordersData = getCollectionFn('orders');
-export const reviewsData = getCollectionFn('reviews');
-
+// export const users = getCollectionFn('users');
+// export const ordersData = getCollectionFn('orders');
+export const products = getCollectionFn('products');
