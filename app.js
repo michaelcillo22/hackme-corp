840ddea03e19443f47243ff931f37a730ceec853
Kayla Hartland <kaylanhartland@gmail.com>
2025-04-07 16:40:27 -0400

    sales routes

--- app.js
+++ app.js
@@ -1,62 +1,31 @@
-import { ConnectionClosedEvent } from 'mongodb';
-import {dbConnection, closeConnection} from './config/mongoConnection.js';
-import { ObjectId } from 'mongodb';
-import * as sales from "./data/sales.js";
+import { dbConnection, closeConnection } from './config/mongoConnection.js';
 
-const db = await dbConnection();
-await db.dropDatabase();
+import express from 'express';
+import { seedDB } from './seed.js';
+const app = express();
+// import configRoutes from './routes/index.js';
 
-let sale1;
-let sale2;
-let sale3;
 
-let sale1buyer;
-let sale1order;
-let sale1Id;
 
-try {
-    let sale1 = await sales.createSale(new ObjectId().toString(), new ObjectId().toString(), "40.00", "credit card", "paid", new ObjectId().toString(), new Date(), ["keyboard"]);
-    sale1Id = sale1._id.toString();
-    sale1buyer = sale1.buyerId;
-    sale1order = sale1.orderId;
-    console.log(sale1);
-} catch (error) {
-    console.log(error);
-}
+(async () => {
+    const db = await dbConnection();
+    console.log('Database connected');
 
-try {
-    let sale2 = await sales.createSale(new ObjectId().toString(), new ObjectId().toString(), "75.54", "paypal", "pending", new ObjectId().toString(), new Date(), ["keyboard", "microphone"]);
-    console.log(sale2);
-} catch (error) {
-    console.log(error);
-}
+    try {
+        await seedDB();
 
-try { //error case
-    let sale3 = await sales.createSale(new ObjectId().toString(), new ObjectId().toString(), "hello", "paypal", "pending", new ObjectId().toString(), new Date(), ["keyboard", "microphone"]);
-    console.log(sale2);
-} catch (error) {
-    console.log(error);
-}
+    } catch (e) {
+        console.error('Errors seeding or server setup:', e);
+    } finally {
+        await closeConnection();
+    }
 
-try { //test get methods
-    let getSale1 = await sales.getSaleByBuyerId(sale1buyer);
-    console.log(getSale1);
-} catch (error) {
-    console.log(error);
-}
+app.use(express.json());
 
-try { //test get methods
-    let getSale2 = await sales.getSaleByOrderId(sale1order);
-    console.log(getSale2);
-} catch (error) {
-    console.log(error);
-}
+// configRoutes(app);
 
-try { //test remove method
-    let test = await sales.removeSale(sale1Id);
-    console.log(test);
-} catch (error) {
-    console.log(error);
-}
-
-await closeConnection();
+app.listen(3000, () => {
+    console.log('Ive now got a server!');
+    console.log('My routes will be running on http://localhost:3000');
+});
+})();
---
+++ routes/sales.js
@@ -0,0 +1,31 @@
+import {Router} from 'express';
+import * as sales from "../data/sales.js"
+import * as helpers from "../helpers_kh.js";
+
+const router = Router();
+
+//get all sales for a business with userId
+router.route('/userId').get(async (req, res) => {
+    
+  });
+
+  //get a specific sale with saleId
+router.route('/userId/id').get(async (req, res) => {
+    
+    })
+//delete a sale with saleId made to a business with userId
+.delete(async (req, res) => {
+
+    });
+
+//get all sales made by buyer with buyerId at business with userId
+router.route('/userId/buyerId').get(async (req, res) => {
+    
+    });
+
+
+
+
+
+//export router
+export default router;
