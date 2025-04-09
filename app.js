85f4f8aa482ed8e8e97da5b4a6d7133deaaa4c49
christendeo <christen.deocampo@gmail.com>
2025-04-09 11:49:06 -0400

    Minor additions to app.js with seed files added

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ import express from 'express';
 import middleWareFunctions from './tasks/middleware.js';
 import router from './routes/index.js';  
 import { dbConnection, closeConnection } from './config/mongoConnection.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Import seed files
+import * as productSeed from './tasks/productSeed.js';
+import * as categorySeed from './tasks/RDCatseed.js';
 
 // Initialize app with express
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Ensure correct directory path
+const fileName = fileURLToPath(import.meta.url);
+const directoryName = path.dirname(fileName);
+
+// For loading CSS and client JS
+app.use('/public', express.static(path.join(directoryName, 'public')));
 
 
 // DB connection
@@ -19,19 +31,24 @@ const PORT = process.env.PORT || 5000;
 
   try {
     //await ; use this for seed files
-    
+    categorySeed.seedDB();
+    productSeed.productSeed();
+
   } catch (e) {
     console.error('Errors seeding or server setup:', e);
-} finally {
-    await closeConnection();
-}
+  }
+  // finally {
+  //   await closeConnection();
+  // }
 })();
 
 // Middleware Configuration
 middleWareFunctions(app);
 
 // Use Routes
-app.use('/', router);
+// app.use('/', router);
+// Call routes with the app instance since it's already imported
+router(app);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -39,5 +56,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:5000`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
