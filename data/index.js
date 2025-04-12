cf5202e63c057ce44a78b47d499a5f9a0278ee15
Michael Cillo <michael@Michaels-MacBook-Pro-2.local>
2025-04-12 16:18:14 -0400

    added ordersData to index.js

--- data/index.js
+++ data/index.js
@@ -1,3 +1,6 @@
 import * as productDataFunctions from './products.js';
+import * as ordersDataFunctions from './orders.js';
+
 
 export const productData = productDataFunctions;
+export const ordersData = ordersDataFunctions;
