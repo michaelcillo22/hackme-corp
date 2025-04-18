442885c7a5c80cff68cebab21d229776202cf26e
christendeo <christen.deocampo@gmail.com>
2025-04-18 05:22:55 -0400

    Updating data index.js and re adding shoppingCart from Rita's collection

--- data/index.js
+++ data/index.js
@@ -1,5 +1,7 @@
 import * as productDataFunctions from './products.js';
 import * as reviewDataFunctions from './reviews.js';
+import * as orderDataFunctions from './orders.js';
 
 export const productInfo = productDataFunctions;
 export const reviewInfo = reviewDataFunctions;
+export const orderInfo = orderDataFunctions;
--- data/shoppingCart.js
+++ data/shoppingCart.js
@@ -1,45 +1,100 @@
-export default class Cart {
-  items = {};
-  totalQty = 0;
-  totalPrice = 0;
+// shoppingCart.js
+import { ObjectId } from "mongodb";
+import { carts } from "../config/mongoCollection.js";
+import helpers from "../helpers_CD.js";
 
-  constructor(oldCart = {}) {
-    Object.assign(this, {
-      items: oldCart.items || {},
-      totalQty: oldCart.totalQty || 0,
-      totalPrice: oldCart.totalPrice || 0,
-    });
-  }
+const shoppingCartMethods = {
+    
+    async getCartByUserId(userId) {
+        userId = helpers.checkString(userId, "User ID");
 
-  add = (item, id) => {
-    const storedItem = this.items[id] ??= { item, qty: 0, price: 0 };
-    storedItem.qty++;
-    storedItem.price = storedItem.item.price * storedItem.qty;
-    this.totalQty++;
-    this.totalPrice += storedItem.item.price;
-  };
+        const cartCollection = await carts();
+        const cart = await cartCollection.findOne({ userId });
 
-  reduceByOne = (id) => {
-    const storedItem = this.items[id];
-    if (!storedItem) return;
+        if (!cart) {
+            return { userId, items: [] }; 
+        }
 
-    storedItem.qty--;
-    storedItem.price -= storedItem.item.price;
-    this.totalQty--;
-    this.totalPrice -= storedItem.item.price;
+        return cart;
+    },
 
-    if (storedItem.qty <= 0) {
-      delete this.items[id];
-    }
-  };
+    // Add item to cart
+    async addItemToCart(userId, productId, quantity) {
+        userId = helpers.checkString(userId, "User ID");
+        productId = helpers.checkId(productId, "Product ID");
 
-  removeItem = (id) => {
-    if (!this.items[id]) return;
+        if (typeof quantity !== "number" || quantity <= 0) {
+            throw new Error("Quantity must be a positive number.");
+        }
 
-    this.totalQty -= this.items[id].qty;
-    this.totalPrice -= this.items[id].price;
-    delete this.items[id];
-  };
+        const cartCollection = await carts();
+        const cart = await cartCollection.findOne({ userId });
 
-  generateArray = () => Object.values(this.items);
-}
+        if (!cart) {
+            // Create a new cart 
+            const newCart = { userId, items: [{ productId, quantity }] };
+            await cartCollection.insertOne(newCart);
+            return newCart;
+        }
+
+        const existingItem = cart.items.find(item => item.productId === productId);
+        if (existingItem) {
+            existingItem.quantity += quantity; 
+        } else {
+            cart.items.push({ productId, quantity });
+        }
+
+        await cartCollection.updateOne({ userId }, { $set: { items: cart.items } });
+        return cart;
+    },
+
+    // Remove item from cart
+    async removeItemFromCart(userId, productId) {
+        userId = helpers.checkString(userId, "User ID");
+        productId = helpers.checkId(productId, "Product ID");
+
+        const cartCollection = await carts();
+        const cart = await cartCollection.findOne({ userId });
+
+        if (!cart) throw new Error("Cart not found.");
+
+        const updatedItems = cart.items.filter(item => item.productId !== productId);
+
+        await cartCollection.updateOne({ userId }, { $set: { items: updatedItems } });
+        return await this.getCartByUserId(userId);
+    },
+
+    // Update quantity of the item in the cart
+    async updateItemQuantity(userId, productId, quantity) {
+        userId = helpers.checkString(userId, "User ID");
+        productId = helpers.checkId(productId, "Product ID");
+
+        if (typeof quantity !== "number" || quantity <= 0) {
+            throw new Error("Quantity must be a positive number.");
+        }
+
+        const cartCollection = await carts();
+        const cart = await cartCollection.findOne({ userId });
+
+        if (!cart) throw new Error("Cart not found.");
+
+        const itemIndex = cart.items.findIndex(item => item.productId === productId);
+        if (itemIndex === -1) throw new Error("Product not found in cart.");
+
+        cart.items[itemIndex].quantity = quantity;
+
+        await cartCollection.updateOne({ userId }, { $set: { items: cart.items } });
+        return cart;
+    },
+
+    async clearCart(userId) {
+        userId = helpers.checkString(userId, "User ID");
+
+        const cartCollection = await carts();
+        await cartCollection.updateOne({ userId }, { $set: { items: [] } });
+
+        return { userId, items: [] };
+    },
+};
+
+export default shoppingCartMethods;
