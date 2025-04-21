335748e64dfb148032befb577f3fb9351f66bd43
Kayla Hartland <kaylanhartland@gmail.com>
2025-04-21 15:40:09 -0400

    new sales routes and updated routes/index.js

--- data/index.js
+++ data/index.js
@@ -1,3 +1,5 @@
 import * as productDataFunctions from './products.js';
+import * as salesDataFunctions from './sales.js';
 
 export const productData = productDataFunctions;
+export const salesData = productDataFunctions;
