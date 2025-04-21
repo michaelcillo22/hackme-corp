b73202b6f193fddb4dc15da4a053ec6edba1c370
christendeo <christen.deocampo@gmail.com>
2025-04-21 13:42:04 -0400

    Updated app.js to include await and unify dropDatabase

--- app.js
+++ app.js
@@ -26,11 +26,13 @@ const PORT = process.env.PORT || 5000;
 
   try {
 
-    userSeed.seedDB();
-    categorySeed.seedDB();
-    productSeed.productReviewSeed();
-    orderSeed.seedOrders();
-    // productSeed.productReviewSeed();
+    await db.dropDatabase();
+    console.log("Database dropped.")
+
+    await userSeed.seedDB();
+    await categorySeed.seedDB();
+    await productSeed.productReviewSeed();
+    await orderSeed.seedOrders();
     //await ; use this for seed files
     
   } catch (e) {
