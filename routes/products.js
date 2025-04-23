60afa5dfebf6a6dd7eed5dd6b5e5efbcd3ceaef9
christendeo <christen.deocampo@gmail.com>
2025-04-23 13:17:54 -0400

    Adding updated product and review routes with auth

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,11 @@
-// To do//import express and express router as shown in lecture code and worked in previous labs.  Import your data functions from /data/movies.js that you will call in your routes below
+//import express and express router as shown in lecture code and worked in previous labs.  Import your data functions from /data/movies.js that you will call in your routes below
 import express from "express";
 const router = express.Router();
 
 import {productInfo} from "../data/index.js";
 import helpers from "../helpers/helpers_CD.js";
 import categories from "../data/categories.js";
+import xss from "xss";
 
 router.route('/').get(async (req, res) => {
   //code here for GET will render the home handlebars file
@@ -14,12 +15,16 @@ router.route('/').get(async (req, res) => {
     // Get our available products
     let productsList = await productInfo.getAllProducts();
 
+    // Get logged-in user info
+    const userLoggedIn = req.isAuthenticated;
+
     // Render to home.handlebars
     res.render("productHome", {
       title: "Available Products",
       productDescription: "Search our available products today!",
       searchInputId: "searchProductsByName",
-      products: productsList
+      products: productsList,
+      user: userLoggedIn
     })
 
   } catch (e) {
@@ -54,7 +59,7 @@ router.route('/searchproducts').post(async (req, res) => {
     productSearchTerm = productData.searchProductsByName;
 
     // Make appropriate validations using checkString
-    productSearchTerm = helpers.checkString(productSearchTerm, "Product Search Term");
+    productSearchTerm = xss(helpers.checkString(productSearchTerm, "Product Search Term"));
   } catch (e) {
     return res.status(400).render("productError", {errorMsg: e.toString()});
   }
@@ -104,51 +109,79 @@ router
   .route('/createproduct')
 
     // Show our create product page handlebar
-      .get(async (req, res) => {
-        try {
-          
-          // Obtain categories dynamically
-          let getAllCategories = await categories.getAllCategories();
-          return res.render("createProduct", {
-            title: "List a New Product",
-            categories: getAllCategories
-          });
-        } catch (e) {
-          return res.status(400).render("productError", { errorMsg: e.toString() });
+    .get(async (req, res) => {
+      try {
+        
+        // Ensure ensure user is auth and logged in
+        if (!req.isAuthenticated) {
+          return res.redirect("/auth/login");
         }
+
+        // Obtain categories dynamically
+        let getAllCategories = await categories.getAllCategories();
+        return res.render("createProduct", {
+          title: "List a New Product",
+          categories: getAllCategories
+        });
+      } catch (e) {
+        return res.status(400).render("productError", { errorMsg: e.toString() });
+      }
     })
+
     .post(async (req, res) => {
-        try {
+      try {
+        
+        // Ensure ensure user is auth and logged in
+        if (!req.isAuthenticated) {
+          return res.redirect("/auth/login");
+        }
 
         // Declare the variables we will be adding as input
-        const {
+        let {
             category,
-            vendor,
             name,
             description,
             price,
             photos,
             condition,
-            status
+            status,
+            stock
         } = req.body;
 
+        // Vendor is the name of user logged in
+        let vendor = req.session.userName;
+
+        // Sanitize and add XSS
+        category = xss(category);
+        name = xss(name);
+        description = xss(description);
+        condition = xss(condition);
+        status = xss(status);
+
+        // Check as number
+        price = Number(xss(price.toString()));
+        stock = Number(xss(stock.toString()));
+
+        // TO DO: Switch to photo upload using Multer or ensure to sanitize each URL in photos
+
         // Call our createProduct method to create method in DB
-        const createNewProduct = await productInfo.createProduct(
+        let createNewProduct = await productInfo.createProduct(
             category,
             vendor,
             name,
             description,
-            parseFloat(price),                          // Ensure price is a number, not string
+            price,                                      // Ensure price is a number, not string
             Array.isArray(photos) ? photos : [photos],  // Convert photos to an array if needed.
             condition,
-            status
+            // status,
+            stock
         );
         
         // Ensure to send new product to JSON, status 201 meaning a new resource, aka product, was successfully created
         res.status(201).json(createNewProduct);
-        } catch (e) {
-            return res.status(400).json({ error: e.toString() });
-        }
+      } catch (e) {
+          return res.status(400).json({ error: e.toString() });
+      }
     });
 
 router.route('/:id').get(async (req, res) => {
@@ -201,6 +234,7 @@ router.route('/:id').get(async (req, res) => {
         photos: productResults.photos,
         condition: productResults.condition,  
         status: productResults.status,    
+        stock: productResults.stock,
         reviews: productResults.reviews,
         overallRating: productResults.overallRatingValue,
         productListedDate: productResults.productListedDate 
