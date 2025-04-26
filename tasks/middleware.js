754c9882a544f173f416013ffeb76d6b5288690f
Richard Devitt <richyrychporter@hotmail.com>
2025-04-25 20:07:19 -0400

    Updated middleware.js

--- tasks/middleware.js
+++ tasks/middleware.js
@@ -7,8 +7,9 @@ import { engine } from 'express-handlebars';
 import { users } from '../config/mongoCollections.js';
 // import { fileURLToPath } from 'url';
 
-// const fileName = fileURLToPath(import.meta.url);
-// const directoryName = path.dirname(fileName);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
 
 
 export default (app) => {
@@ -74,73 +75,11 @@ export default (app) => {
     });
     
 
-//     app.use('/', async (req, res, next) => {
-//         console.log('Middleware processing started');
 
 
-//         if (req.originalUrl.startsWith('/public')) {
-//             console.log('Public route detected, skipping middleware');
-//             return next();
-//         }
-
-//         else if (req.originalUrl.startsWith('/auth')) {
-           
-//         try {
-//             const timestamp = new Date().toUTCString();
-//             req.isAuthenticated = req.session && req.session.userId;
-//             req.userType = null;
-    
-//             if (req.isAuthenticated) {
-//                 if (!req.session.userType) {
-//                 const usersCollection = await users();
-//                 const user = await usersCollection.findOne({ _id: new ObjectId(req.session.userId) });
-            
-//                 req.session.userType = user ? user.userType : null;
-//                 } 
-                
-//             }
-        
-//         console.log(`Current Timestamp: ${timestamp}, Request Method: ${req.method}, Request Route: ${req.originalUrl}`);
-//         if (req.isAuthenticated) {
-//             console.log('User is authenticated');
-//         } else {
-//             console.log('User has not been authenticated');
-//         }
-    
-//         res.locals.isAuthenticated = req.isAuthenticated;
-//         res.locals.userType = req.userType;
-
-//         if (req.isAuthenticated && req.originalUrl === '/auth/login') {
-//             console.log('Authenticated user trying to access login, redirected to home');
-//             return res.redirect('/auth/home');
-//         }
-    
-//         else if (req.isAuthenticated && !['/auth/logout', '/auth/error', '/auth/login', '/auth/register'].includes(req.originalUrl)) {
-//             console.log('Redirecting authenticated user to home');
-//             return res.redirect('/auth/home');
-//         }  
-    
-//         next();   
-    
-//     } catch (error) {
-//         console.error('Error in middleware:', error);
-//         res.status(500).send('Internal Server Error');
-//     }     
-// } else {
-//     return next();
-// }
-    // });
-
-     
- 
-
-
- // Response compression
-
-
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', path.join(path.resolve(), 'views'));
-app.get('/favicon.ico', (req, res) => res.status(204).end());
+    app.engine('handlebars', engine());
+    app.set('view engine', 'handlebars');
+    app.set('views', path.join(path.resolve(), 'views'));
+    app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 };
