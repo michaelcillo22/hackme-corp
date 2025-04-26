9305bb809467f86f48ebe851dc1ff00bf60e236f
Michael Cillo <mjcillo22@gmail.com>
2025-04-25 21:25:25 -0400

    routes/index

--- routes/index.js
+++ routes/index.js
@@ -5,17 +5,17 @@ import authRoutes from './auth.js';
 import categoriesRoutes from './categories.js';
 import reviewRoutes from './reviews.js';
 import salesRoutes from './sales.js';
-<<<<<<< Updated upstream
+
 import shoppingCartRoutes from './shoppingCart.js';
-=======
-import dashboardRoutes from './dashboard.js'
+
+//import dashboardRoutes from './dashboard.js'
 // import shoppingCartRoutes from './shoppingCart.js';
->>>>>>> Stashed changes
+
 // import checkoutRoutes from './checkout.js';
 import path from 'path';
 
 const constructorMethod = (app) => {
-<<<<<<< Updated upstream
+
 
     // This will ensure that the login state when logged in passes through every page
     app.use(async (req, res, next) => {
@@ -31,8 +31,8 @@ const constructorMethod = (app) => {
         next();
     });
 
-=======
->>>>>>> Stashed changes
+
+
     app.use('/users', userRoutes);
     app.use('/orders', orderRoutes);
     app.use('/products', productRoutes);
@@ -41,15 +41,8 @@ const constructorMethod = (app) => {
     app.use('/cart', shoppingCartRoutes);
     // app.use('/checkout', checkoutRoutes);
     app.use('/auth', authRoutes);
-<<<<<<< Updated upstream
     app.use('/sales', salesRoutes);
-    app.get('/payment_index', (req, res) => {
-        res.sendFile(path.resolve('static/payment_index.html'));
-    });
-=======
-    app.use('/sales', salesRoutes)
-    app.use('/dashboard', dashboardRoutes);
->>>>>>> Stashed changes
+    
     app.use(/(.*)/, (req, res) => {
         return res.status(404).json({ error: 'Not found'});
     });
